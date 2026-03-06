8d2147e114faa8994e2c82436e4f18bf76f1ec61
Manjot Kaur <138082401+manjotkaurr31@users.noreply.github.com>
2026-03-06 20:07:40 +0530

    running total moving average

---
+++ analysis/cumulative_analysis.sql
@@ -0,0 +1,70 @@
+-- Displays cumulative revenue growth over time using a running total of monthly sales.
+SELECT
+order_month,
+total_sales,
+SUM(total_sales) OVER (ORDER BY order_month) AS running_total_sales
+FROM
+(
+SELECT
+DATETRUNC(MONTH, order_date) AS order_month,
+SUM(sales_amount) AS total_sales
+FROM gold.fact_sales
+where order_date IS NOT NULL
+GROUP BY DATETRUNC(MONTH,order_date)
+) t
+ORDER BY order_month;
+GO
+
+-- Displays cumulative order volume over time using a running total of monthly orders.
+SELECT
+order_month,
+total_orders,
+SUM(total_orders) OVER (ORDER BY order_month) AS running_total_orders
+FROM
+(
+SELECT
+DATETRUNC(MONTH, order_date) AS order_month,
+COUNT(order_number) AS total_orders
+FROM gold.fact_sales
+WHERE order_date IS NOT NULL
+GROUP BY DATETRUNC(MONTH, order_date)
+) t
+ORDER BY order_month;
+GO
+
+-- Displays cumulative product demand over time using a running total of units sold.
+SELECT
+order_month,
+total_units_sold,
+SUM(total_units_sold) OVER (ORDER BY order_month) AS running_total_units_sold
+FROM
+(
+SELECT
+DATETRUNC(MONTH, order_date) AS order_month,
+SUM(quantity) AS total_units_sold
+FROM gold.fact_sales
+WHERE order_date IS NOT NULL
+GROUP BY DATETRUNC(MONTH, order_date)
+) t
+ORDER BY order_month;
+GO
+
+-- Displays a moving average of monthly sales to smooth short-term revenue fluctuations.
+SELECT
+order_month,
+total_sales,
+AVG(total_sales) OVER (
+ORDER BY order_month
+ROWS BETWEEN 2 PRECEDING AND CURRENT ROW
+) AS moving_avg_3_month_sales
+FROM
+(
+SELECT
+DATETRUNC(MONTH, order_date) AS order_month,
+SUM(sales_amount) AS total_sales
+FROM gold.fact_sales
+WHERE order_date is not null
+GROUP BY DATETRUNC(MONTH, order_date)
+) t
+ORDER BY order_month;
+GO
